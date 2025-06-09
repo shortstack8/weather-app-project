07ce6ab7eeb0c71d3c9852e187d4ca39c93781da
Tirelle P <tirelle.peter@icloud.com>
2025-06-09 13:14:58 +1000

    Injected more weather data via API functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 function updateTemperature(response) {
-  let currentTemperatureElement = document.querySelector("#current-temperature");
+  let currentTemperatureElement = document.querySelector(
+    "#current-temperature"
+  );
   let temperature = response.data.temperature.current;
-  let locationElement = document.querySelector("#location");
 
-  locationElement.innerHTML = response.data.city;
+  let locationElement = document.querySelector("#location");
+  let location = response.data.city;
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  let weatherDescription = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let windSpeed = response.data.wind.speed;
+
+  console.log(response.data);
+
   currentTemperatureElement.innerHTML = Math.round(temperature);
+  locationElement.innerHTML = location;
+  timeElement.innerHTML = formatDate(date);
+  weatherDescriptionElement.innerHTML = weatherDescription;
+  humidityElement.innerHTML = `${humidity}%`;
+  windSpeedElement.innerHTML = `${Math.round(windSpeed)}km/h`;
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
